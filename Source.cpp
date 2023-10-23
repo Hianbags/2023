82e9e7168aed328115bb993000e14d50447a64a2
nguyen <trannhunguyen78@gmail.com>
2023-10-23 14:29:19 +0700

    hieu va thuong

--- Source.cpp
+++ Source.cpp
@@ -2,12 +2,25 @@
 
 using namespace std;
 int tong(int a, int b);
-int hieu(int a, int b);
+int hieu(int a, int b){
+	return a-b;
+}
 int tich(int a, int b);
-float thuong(int a, int b);
+float thuong(int a, int b){
+	return a/b;
+}
 int main()
-{
-	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
+{	
+	int a,b;
+	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT"<<endl;
+	cout<<"cho nam nhap so a = ";
+	cin>>a;
+	cout<<"cho nam nhap so b = ";
+	cin>>a;
+	int hieu =hieu(a,b);
+	int tong =tinh(a,b);
+	cout<< "tong = "<<tong;
+	cout<<"hieu = "<<hieu;
 	system("pause");
 	return 0;
 }
